652be3162d4b94ec7773f79d0777e29588618311
Vitaliy Bohdan <vitaliy.flashnika@gmail.com>
2019-01-14 23:05:06 +0200

    fix responsive bug, add close function on grey area, fix image scale

--- src/js/index.js
+++ src/js/index.js
@@ -16,8 +16,13 @@ const closeBtn = document.querySelector(".button-close");
 const prewImg = document.querySelector(".button-left");
 const nextImg = document.querySelector(".button-right");
 const favorit = document.querySelector(".button-favorit");
+const layer = document.querySelector(".modal");
+layer.addEventListener("click", function(e) {
+  closeModal(e);
+});
+
 let favoriteCollections = [];
-if(storage.get()){
+if (storage.get()) {
   favoriteCollections = [...storage.get()];
 }
 
@@ -31,7 +36,7 @@ loadMoreBtn.addEventListener("click", handleLoadMoreClick);
 
 nextImg.addEventListener("click", showNextImg);
 prewImg.addEventListener("click", showPrew);
-closeBtn.addEventListener("click", closeModal);
+// closeBtn.addEventListener("click", closeModal);
 favorit.addEventListener("click", addToFavorit);
 
 // Helpers
@@ -47,12 +52,11 @@ function hydratePhotosGrid(photos, cb) {
   updatePhotosGrid(markup);
 }
 function isAdded(id) {
-  if(storage.get() !== null){
+  if (storage.get() !== null) {
     return storage.get().find(elem => elem.id == id);
-  }else {
+  } else {
     return false;
   }
- 
 }
 /**
  *
@@ -207,8 +211,16 @@ function onFavClick(e) {
 
 //functions for modal window (New line)
 
-function closeModal() {
-  modal.style.display = "none";
+function closeModal(e) {  
+  e.stopPropagation();
+  if (
+    e.target.classList.contains("button-close") ||
+    e.target.classList.contains("modal")
+  ) {
+    modal.style.display = "none";
+  } else {
+    return;
+  }
 }
 
 function showNextImg() {
